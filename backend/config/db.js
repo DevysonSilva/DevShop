f10848cc0120f53b7ff6d2da452a3ea8189ae556
devysonsilva <devysonssilva@gmail.com>
2025-06-01 16:57:21 -0300

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
-// backend/config/db.js
+// config/db.js
+
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'LmDTvYfirIEMveucbORPUpCEELvulOOa',
+  database: 'railway',
+  port: 3306,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
